1b59dc68cb613ae67a240d1171047b50681812e5
Yashmita Sharma <71992108+xyashmitax@users.noreply.github.com>
2024-11-14 08:33:17 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let toggle = document.querySelector("#header .toggle-button");
+let collapse = document.querySelectorAll("#header .collapse");
+
+toggle.addEventListener('click' , function(){
+     collapse.forEach(col=>col.classList.toggle("collapse-toggle"));
+})
+
+//with masonry
+/*new Masonry("#posts .grid", {
+     itemSelector :'.grid-item',
+     gutter : 20
+})*/
